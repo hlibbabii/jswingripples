8b7532bac4a1e7a4a10626eeec2dd956da74d875
Sebastián Pujá López <sebastian.puja.lopez@gmail.com>
2016-11-24 11:57:37 -0300

    Finished GitHub Settings ui. Just for demo2 this put all issues of the given repo on standard out.

--- jswingripples/src/main/java/org/incha/ui/GithubSettings.java
+++ jswingripples/src/main/java/org/incha/ui/GithubSettings.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.List;
 
 import org.kohsuke.github.*;
 
@@ -13,36 +14,37 @@ import org.kohsuke.github.*;
  */
 public class GithubSettings {
 
-    private static final JPanel configPanel = new JPanel();
+    private static final JPanel configPanel = new JPanel(new BorderLayout());
     private static final JTextField url = new JTextField(20);
-    //private static final JLabel crntRepo = new JLabel("Current Repository: ");
+    private static final JLabel crntRepo = new JLabel("Current Repository: ");
     private static final JButton testConnection = new JButton("Test");
     private static final JButton connect = new JButton("Connect");
+    private static String currentRepo;
 
     public static JPanel getConfigPanel(){
+    	
+    	currentRepo = obtainCurrentRepo(null);
+    	crntRepo.setText(crntRepo.getText() + currentRepo);
 
-        configPanel.setLayout(new BoxLayout(configPanel, BoxLayout.Y_AXIS));
-        //JPanel north = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel north = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JPanel center = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        JPanel south = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-        //north.add(new JLabel("Connect to Github repository"),BorderLayout.WEST);
-        center.add(new JLabel("Github URL repository: "), BorderLayout.WEST);
-        center.add(url, BorderLayout.EAST);
-        center.add(testConnection, BorderLayout.EAST);
-        center.add(connect, BorderLayout.EAST);
-        //center.add()
-        //south.add(crntRepo,BorderLayout.WEST);
+        north.add(new JLabel("Github URL repository: "), BorderLayout.WEST);
+        north.add(url, BorderLayout.EAST);
+        north.add(testConnection, BorderLayout.EAST);
+        north.add(connect, BorderLayout.EAST);
+        
+        center.add(crntRepo,BorderLayout.WEST);
 
-        configPanel.add(center);
-        configPanel.add(south);
+        configPanel.add(north, BorderLayout.NORTH);
+        configPanel.add(center, BorderLayout.CENTER);
 
-        //addButtonListeners(testConnection,connect);
+        addButtonListeners(testConnection,connect);
 
         return configPanel;
     }
 
-/*    private static void addButtonListeners(JButton testConnection, JButton connect){
+    private static void addButtonListeners(JButton testConnection, JButton connect){
 
         testConnection.addActionListener(new ActionListener() {
             @Override
@@ -55,15 +57,46 @@ public class GithubSettings {
             @Override
             public void actionPerformed(ActionEvent e) {
                 GitHub github = null;
+                GHRepository myGHRep;
                 try {
                     github = GitHub.connectAnonymously();
-                    GHRepository myGHRep = github.getRepository("JSwingRipples2016/jswingripples");
+                    String repo = formatURLRepo(url.getText());
+                    url.setText("");                    
+                    if (repo.compareTo("Error") == 0){
+                    	return;
+                    }
+                    myGHRep = github.getRepository(repo);
+                    
+                    /* ---- Just for demo2 ----*/
+                    List<GHIssue> issues = myGHRep.getIssues(GHIssueState.ALL);
+                    for (GHIssue issue : issues){
+                    	System.out.println(issue.getTitle());
+                    }                    
+                    crntRepo.setText("Current Repository: " + repo);
+                    /* ----------------------- */
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
-                System.out.println(myGHRep.getDescription());
+                
             }
         });
-    }*/
+    }
+    
+    /**
+     * This method should look for the current repository for the project
+     * If there is no, return empty String 
+     */
+    private static String obtainCurrentRepo(String projectName){
+    	return "";
+    }
+    
+    private static String formatURLRepo (String urlRepo){
+    	if(urlRepo.startsWith("https://github.com/")){
+    		return urlRepo.split("https://github.com/")[1];
+    	} else if (urlRepo.startsWith("http://github.com/")){
+    		return urlRepo.split("http://github.com/")[1];
+    	}
+    	return "Error";
+    			 
+    }
 }
-
