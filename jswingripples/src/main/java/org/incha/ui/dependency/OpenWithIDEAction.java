f654ee93c12f1bd52c6b28bf6f21b9f7e5e726cf
jacquems <jacquems@gmail.com>
2016-11-25 18:42:20 -0300

    Show option "open with IDE". Updates in the new branch

--- jswingripples/src/main/java/org/incha/ui/dependency/OpenWithIDEAction.java
+++ jswingripples/src/main/java/org/incha/ui/dependency/OpenWithIDEAction.java
@@ -1,29 +1,41 @@
 package org.incha.ui.dependency;
 
+import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.FileSystem;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+import org.incha.core.JavaProject;
 
 import org.incha.core.jswingripples.eig.JSwingRipplesEIGNode;
 import org.incha.ui.JSwingRipplesApplication;
+import org.incha.utils.FileVisitor;
+import org.incha.utils.IoUtils;
+import java.util.regex.Matcher;
 
 public class OpenWithIDEAction extends AbstractAction {
     private static final long serialVersionUID = 6537463394107510663L;
-    /**
-     * Node.
-     */
     private final JSwingRipplesEIGNode node;
 
     /**
      * @param node node.
      */
     public OpenWithIDEAction(final JSwingRipplesEIGNode node) {
-        super("Open with IDE");
+        super("Open with Editor");
         this.node = node;
-        this.enabled = false;
+    }
+    
+    
+    public void showErrorMessage(){
+        JOptionPane.showMessageDialog(null, "It is not possible to open the file with the default editor.");
     }
 
     /* (non-Javadoc)
@@ -31,6 +43,22 @@ public class OpenWithIDEAction extends AbstractAction {
      */
     @Override
     public void actionPerformed(final ActionEvent e) {
-        
+        try{
+            
+            File file = new File(node.getAbsolutePath().replaceAll("/", Matcher.quoteReplacement(File.separator)));
+            Desktop desktop = Desktop.getDesktop();
+            if (!Desktop.isDesktopSupported()) {
+                showErrorMessage();
+            }
+            
+            if (desktop.isSupported(Desktop.Action.EDIT)) {
+                desktop.edit(file);
+            } else if (desktop.isSupported(Desktop.Action.OPEN)){
+                desktop.edit(file);
+            }
+            
+        } catch (Exception ex){
+            showErrorMessage();
+        }
     }
 }
