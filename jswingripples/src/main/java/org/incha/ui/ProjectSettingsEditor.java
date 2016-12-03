18c4fab3526b322da465c290873d6206a3625384
jestebanlh <joseesteban.lopezh@gmail.com>
2016-12-03 14:03:08 -0300

    -Some code format -Have been changed the name of two variables

--- jswingripples/src/main/java/org/incha/ui/ProjectSettingsEditor.java
+++ jswingripples/src/main/java/org/incha/ui/ProjectSettingsEditor.java
@@ -10,7 +10,9 @@ import javax.swing.JTabbedPane;
 import org.incha.core.JavaProject;
 
 public class ProjectSettingsEditor extends JTabbedPane {
+    
     private static final long serialVersionUID = 6521933918476824719L;
+    
     /**
      * The java project.
      */
@@ -23,7 +25,6 @@ public class ProjectSettingsEditor extends JTabbedPane {
         super(LEFT);
         setTabLayoutPolicy(WRAP_TAB_LAYOUT);
         this.project = project;
-
         addTab("Project", createProjectSettingsTab());
         addTab("Build Path", createBuildPathTab());
         addTab("Github", createGithubTab());
@@ -34,29 +35,28 @@ public class ProjectSettingsEditor extends JTabbedPane {
      */
     private JPanel createProjectSettingsTab() {
         final JPanel root = new JPanel(new FlowLayout(FlowLayout.LEADING));
-
         final JPanel panel = new JPanel(new GridLayout(2, 1));
         //project name
         panel.add(new JLabel("Name: " + project.getName()));
-
         root.add(panel);
         return root;
     }
+    
     /**
      * @return
      */
     private JTabbedPane createGithubTab() {
-        final JTabbedPane tp = new JTabbedPane(TOP, WRAP_TAB_LAYOUT);
-        tp.addTab("GitHub Settings", new GitHubSettings(project));
-        return tp;
+        final JTabbedPane tabPane = new JTabbedPane(TOP, WRAP_TAB_LAYOUT);
+        tabPane.addTab("GitHub Settings", new GitHubSettings(project));
+        return tabPane;
     }
 
     /**
      * @return
      */
     private JTabbedPane createBuildPathTab() {
-        final JTabbedPane tp = new JTabbedPane(TOP, WRAP_TAB_LAYOUT);
-        tp.addTab("Sources", new SourcesEditor(project));
-        return tp;
+        final JTabbedPane tabPane = new JTabbedPane(TOP, WRAP_TAB_LAYOUT);
+        tabPane.addTab("Sources", new SourcesEditor(project));
+        return tabPane;
     }
 }
