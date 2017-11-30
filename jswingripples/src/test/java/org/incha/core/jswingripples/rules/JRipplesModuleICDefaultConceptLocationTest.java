a3d4272b030f7a7aafc779aa6882c4cb1aaa9c73
Hlib Babii <hlib.babii@unibz.it>
2017-11-30 09:05:01 +0100

    RS-504: add unit-test for JRipplesModuleICDefaultConceptLocation

---
+++ jswingripples/src/test/java/org/incha/core/jswingripples/rules/JRipplesModuleICDefaultConceptLocationTest.java
@@ -0,0 +1,35 @@
+package org.incha.core.jswingripples.rules;
+
+import org.incha.ui.jripples.EIGStatusMarks;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+import static org.junit.Assert.assertEquals;
+
+public class JRipplesModuleICDefaultConceptLocationTest {
+    @Test
+    public void getAvailableRulesForMark() throws Exception {
+        JRipplesModuleICDefaultConceptLocation cp
+                = new JRipplesModuleICDefaultConceptLocation(null);
+
+        assertEquals(null, cp.GetAvailableRulesForMark(null));
+        assertEquals(null, cp.GetAvailableRulesForMark(EIGStatusMarks.BLANK));
+        assertEquals(new LinkedHashSet<>(Arrays.asList(
+                EIGStatusMarks.LOCATED, EIGStatusMarks.VISITED_CONTINUE, EIGStatusMarks.VISITED
+        )), cp.GetAvailableRulesForMark(EIGStatusMarks.NEXT_VISIT));
+
+        assertEquals(new LinkedHashSet<>(Arrays.asList(
+                EIGStatusMarks.LOCATED
+        )), cp.GetAvailableRulesForMark(EIGStatusMarks.LOCATED));
+        assertEquals(null, cp.GetAvailableRulesForMark(EIGStatusMarks.CHANGED));
+
+        assertEquals(new LinkedHashSet<>(Arrays.asList(
+                EIGStatusMarks.LOCATED, EIGStatusMarks.VISITED_CONTINUE
+        )), cp.GetAvailableRulesForMark(EIGStatusMarks.VISITED_CONTINUE));
+
+        assertEquals(null, cp.GetAvailableRulesForMark("unknown_mark"));
+    }
+
+}
