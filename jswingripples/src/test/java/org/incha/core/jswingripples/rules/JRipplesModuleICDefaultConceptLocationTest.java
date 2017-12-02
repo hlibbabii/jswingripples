fc025d54db850ee0cd38eaa036812d56e21f57ad
Hlib Babii <hlib.babii@unibz.it>
2017-12-02 22:26:05 +0100

    RS-504: add unit-test

--- jswingripples/src/test/java/org/incha/core/jswingripples/rules/JRipplesModuleICDefaultConceptLocationTest.java
+++ jswingripples/src/test/java/org/incha/core/jswingripples/rules/JRipplesModuleICDefaultConceptLocationTest.java
@@ -1,13 +1,24 @@
 package org.incha.core.jswingripples.rules;
 
+import org.incha.core.jswingripples.eig.JSwingRipplesEIG;
+import org.incha.core.jswingripples.eig.JSwingRipplesEIGNode;
 import org.incha.ui.jripples.EIGStatusMarks;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(CommonEIGRules.class)
 public class JRipplesModuleICDefaultConceptLocationTest {
     @Test
     public void getAvailableRulesForMark() throws Exception {
@@ -31,6 +42,67 @@ public class JRipplesModuleICDefaultConceptLocationTest {
         )), cp.GetAvailableRulesForMark(EIGStatusMarks.VISITED_CONTINUE));
 
         assertEquals(null, cp.GetAvailableRulesForMark("unknown_mark"));
+    }
+
+    @Test
+    public void testApplyRuleAtNodeWithVisitedContinueRule() {
+        /* given */
+        JSwingRipplesEIG eig = mock(JSwingRipplesEIG.class);
+        JSwingRipplesEIGNode node = mock(JSwingRipplesEIGNode.class);
+
+        JRipplesModuleICDefaultConceptLocation cp
+                = new JRipplesModuleICDefaultConceptLocation(eig);
+
+        PowerMockito.mockStatic(CommonEIGRules.class);
+
+        /* when */
+        cp.ApplyRuleAtNode(EIGStatusMarks.VISITED_CONTINUE, node, 0);
+
+        /* then*/
+        PowerMockito.verifyStatic(CommonEIGRules.class);
+        CommonEIGRules.applyRuleToNode(eig, node, EIGStatusMarks.VISITED_CONTINUE, 0);
+
+    }
+
+    @Test
+    public void testApplyRuleAtNodeWithVisitedRule() {
+        /* given */
+        JSwingRipplesEIG eig = mock(JSwingRipplesEIG.class);
+        JSwingRipplesEIGNode node = mock(JSwingRipplesEIGNode.class);
+
+        JRipplesModuleICDefaultConceptLocation cp
+                = new JRipplesModuleICDefaultConceptLocation(eig);
+
+        PowerMockito.mockStatic(CommonEIGRules.class);
+
+        /* when */
+        cp.ApplyRuleAtNode(EIGStatusMarks.VISITED, node, 0);
+
+        /* then*/
+        PowerMockito.verifyStatic(CommonEIGRules.class);
+        CommonEIGRules.applyRuleToNode(eig, node, EIGStatusMarks.VISITED, 0);
+    }
+
+    @Test
+    public void testApplyRuleAtNodeWithLocatedRule() {
+        /* given */
+        JSwingRipplesEIG eig = mock(JSwingRipplesEIG.class);
+        JSwingRipplesEIGNode node = mock(JSwingRipplesEIGNode.class);
+
+        JRipplesModuleICDefaultConceptLocation cp
+                = new JRipplesModuleICDefaultConceptLocation(eig);
+
+        PowerMockito.mockStatic(CommonEIGRules.class);
+
+        /* when */
+        cp.ApplyRuleAtNode(EIGStatusMarks.LOCATED, node, 0);
+
+        /* then*/
+        PowerMockito.verifyStatic(CommonEIGRules.class);
+        CommonEIGRules.assignMarkToNodeAndParents(eig, node, EIGStatusMarks.LOCATED);
+
+        PowerMockito.verifyStatic(CommonEIGRules.class);
+        CommonEIGRules.assignAnottationToNodeAndParents(eq(eig), eq(node), anyString());
     }
 
 }
